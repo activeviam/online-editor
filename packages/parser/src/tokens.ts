7c5cf37085743056fc7887616dd4588709ba340c
stephanoumenos <bous@stephanoumenos.xyz>
2020-12-16 17:40:22 +0100

    Add tokens.ts to represent token data through a class

---
+++ packages/parser/src/tokens.ts
@@ -0,0 +1,34 @@
+/* reference: https://github.com/guidobouman/typescript/blob/master/src/compiler/tokens.ts
+   completed as needed. */
+
+export enum TokenID {
+  // Keywords
+  Const,
+  // Punctuation
+  Equals,
+  Identifier,
+  NumberLiteral,
+  Whitespace
+}
+
+export class TokenInfo {
+  constructor (public tokenId: TokenID, public text: string) { }
+}
+
+export const lookupToken = (tokenId: TokenID): TokenInfo => {
+  return tokenTable.get(tokenId)!;
+}
+
+export var tokenTable = new Map<TokenID, TokenInfo>();
+
+const setTokenInfo = (tokenId: TokenID, text: string) => {
+  if (tokenId !== undefined) {
+    tokenTable.set(tokenId, new TokenInfo(tokenId, text));
+  }
+}
+
+setTokenInfo(TokenID.Const, "const");
+setTokenInfo(TokenID.Equals, "=");
+setTokenInfo(TokenID.Identifier, "identifier");
+setTokenInfo(TokenID.NumberLiteral, "numberLiteral");
+setTokenInfo(TokenID.Whitespace, " ");
