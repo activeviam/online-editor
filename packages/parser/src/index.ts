c45873feaa562a54d1c1ed0b956214b28b9ad374
stephanoumenos <bous@stephanoumenos.xyz>
2020-12-16 17:40:22 +0100

    Add tokens.ts to represent token data through a class

--- packages/parser/src/index.ts
+++ packages/parser/src/index.ts
@@ -1,2 +1,11 @@
 export { parse } from "./parse";
+export {
+  lookupToken,
+  TokenID,
+  TokenInfo,
+  Token,
+  NumberLiteralToken,
+  IdentifierToken,
+  WhitespaceToken,
+} from "./tokens";
 export type { Name } from "./parse";
