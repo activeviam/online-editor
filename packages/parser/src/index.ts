7cffcb8540e42c8b544a4af9d2c9bdf669ab8800
stephanoumenos <bous@stephanoumenos.xyz>
2021-03-03 11:40:40 +0100

    Revert "Add tokens.ts to represent token data through a class (#10)"
    This reverts commit ffe5a1197e329e48e3a1b2ec916ab9ab3a1e2e22.

--- packages/parser/src/index.ts
+++ packages/parser/src/index.ts
@@ -1,12 +1,3 @@
 export { parse } from "./parse";
-export {
-  lookupToken,
-  TokenID,
-  TokenInfo,
-  Token,
-  NumberLiteralToken,
-  IdentifierToken,
-  WhitespaceToken,
-} from "./tokens";
 export { tokenize } from "./tokenize";
 export type { Name } from "./parse";
