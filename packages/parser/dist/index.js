2e47cde00b2f9c27770311140d557ea9b8ca51c1
stephanoumenos <bous@stephanoumenos.xyz>
2021-03-03 11:40:43 +0100

    Revert "Tokenize constant integer assignment"
    This reverts commit 073466904576e4c6fa458bc9bb72946e2b9f65f2.

--- packages/parser/dist/index.js
+++ packages/parser/dist/index.js
@@ -9,13 +9,5 @@ Object.defineProperty(exports, "parse", {
     return _parse.parse;
   }
 });
-Object.defineProperty(exports, "tokenize", {
-  enumerable: true,
-  get: function () {
-    return _tokenize.tokenize;
-  }
-});
 
 var _parse = require("./parse");
-
-var _tokenize = require("./tokenize");
--- packages/parser/src/index.ts
+++ packages/parser/src/index.ts
@@ -1,3 +1,2 @@
 export { parse } from "./parse";
-export { tokenize } from "./tokenize";
 export type { Name } from "./parse";
