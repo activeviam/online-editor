180457529a238e846661c1c505ad97b7218116d0
moad.fethallah <moad.fethallah@student.ecp.fr>
2021-03-04 12:39:13 +0100

    Route for grammar upload

--- packages/parser/index.ts
+++ packages/parser/index.ts
@@ -1,13 +1,26 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import dotenv from "dotenv";
 
-import grammarRouter from "./routes/grammar";
+import session from "express-session";
+import redis from "redis";
+import connectRedis from "connect-redis";
+
+import grammarRouter from "./routes/grammarRoutes";
+
+dotenv.config();
+const RedisStore = connectRedis(session);
+const redisClient = redis.createClient();
 
 // Constants
 
-const PORT = process.env.PORT || 5000;
-
+const PORT = process.env.PORT === undefined ? 5000 : process.env.PORT;
+const SESSION_SECRET = process.env.SESSION_SECRET || "";
+const COOKIE_MAXAGE =
+  process.env.COOKIE_MAXAGE === undefined
+    ? 3600000
+    : parseInt(process.env.COOKIE_MAXAGE);
 const app = express();
 
 // Middlewares
@@ -20,8 +33,31 @@ app.use(
 
 app.use(bodyParser.json());
 
+app.use(
+  session({
+    secret: SESSION_SECRET,
+    saveUninitialized: true,
+    name: "custom-ide",
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      sameSite: false,
+      maxAge: COOKIE_MAXAGE,
+    },
+    store: new RedisStore({ client: redisClient, ttl: 86400 }),
+    resave: false,
+  }),
+);
+
+app.use((req, res, next) => {
+  console.log(req.session, req.sessionID);
+  next();
+});
+
 // Routes
+
 app.use("/grammar", grammarRouter);
+
 // Start server
 
 app.listen(PORT, () => {
