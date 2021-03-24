c50e172de5847fcb59968efda464aed56083e63e
moad.fethallah <moad.fethallah@student.ecp.fr>
2021-03-24 16:56:46 +0100

    return list of defined tokens on grammar compilation

--- packages/parser/controllers/grammarController.ts
+++ packages/parser/controllers/grammarController.ts
@@ -11,13 +11,14 @@ Handlebars.registerHelper("toTitleCase", (str: string) => {
 });
 
 import dotenv from "dotenv";
+import { generateAstTreeFile, generateLexerFile, generateParserFile } from "./grammarTemplatesGenerator";
 dotenv.config();
 
 // Constants
 const ANTLR4TS = process.env.ANTLR4TS || "";
 const ANTLR_FLAGS = process.env.ANTLR_FLAGS || "";
 const GRAMMAR_STORAGE = process.env.GRAMMAR_STORAGE || "grammar";
-const ASSETS = process.env.ASSETS || "assets";
+
 
 // File Upload handlers
 const storage = multer.diskStorage({
@@ -38,87 +39,50 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage }).single("file");
 
+const getGrammarTokens = async (sessionID: string, grammarName: string) : Promise<string[]> => {
+  const grammarLexer = await import(
+    `../grammar/${sessionID}/${grammarName}Lexer.ts`
+  );
+  const lexerClass = grammarLexer[`${grammarName}Lexer`];
+  return lexerClass.ruleNames;
+}
+
 // Compiling Grammar to parser and lexer
 const runAntlr = (
   filename: string,
   grammarPath: string,
   grammarRoot: string,
-) => {
+) : Promise<boolean> => {
+  
+  return new Promise((resolve, reject) => {
   const directory = path.dirname(grammarPath);
   const grammarName = filename.split(".")[0];
 
   // Compile the grammar. The compilation flags are set in the .env file
   const child = spawn(ANTLR4TS, [...ANTLR_FLAGS.split(" "), grammarPath]);
   
-  /*
+  
   child.stdout.on("data", (data) => console.log(data.toString()));
   child.stderr.on("data", (data) => console.log(data.toString()));
-  */
 
   child.on("exit", () => {
     generateParserFile(grammarName, directory);
     generateLexerFile(grammarName, grammarRoot, directory);
     generateAstTreeFile(grammarName, grammarRoot, directory);
+    resolve(true)
   });
-};
-
-// Create grammar related functions
-const generateParserFile = (grammarName: string, directory: string) => {
-  // Generate a function that returns the AST of a given code
-  const parserTemplateFile = fs.readFileSync(`${ASSETS}/parser.hbs`).toString();
-  const parserTemplate = Handlebars.compile(parserTemplateFile);
-
-  const source = parserTemplate({
-    grammarName: grammarName,
-  });
-  fs.writeFileSync(path.join(directory, "parser.ts"), source);
-};
-
-const generateLexerFile = (
-
-  grammarName: string,
-  grammarRoot: string,
-  directory: string,
-) => {
-  // Generate a function that returns the AST of a given code
-  const lexerTemplateFile = fs.readFileSync(`${ASSETS}/lexer.hbs`).toString();
-  const lexerTemplate = Handlebars.compile(lexerTemplateFile);
-
-  const source = lexerTemplate({
-    grammarName: grammarName,
-    grammarRoot: grammarRoot,
-  });
-  fs.writeFileSync(path.join(directory, "lexer.ts"), source);
-};
-
-const generateAstTreeFile = (
-  grammarName: string,
-  grammarRoot: string,
-  directory: string,
-) => {
-  // Generate a function that returns the AST of a given code
-  const parseTreeTemplateFile = fs
-    .readFileSync(`${ASSETS}/parseTree.hbs`)
-    .toString();
-  const parseTreeTemplate = Handlebars.compile(parseTreeTemplateFile);
-
-  const source = parseTreeTemplate({
-    grammarName: grammarName,
-    grammarRoot: grammarRoot,
-  });
-  fs.writeFileSync(path.join(directory, "parseTree.ts"), source);
-
+  })
 };
 
 // Routes controllers
 
 export const compileGrammarFile = (req: any, res: any) => {
-  upload(req, res, (err: any) => {
+  upload(req, res, async (err: any) => {
     if (err) {
       return res.status(500).json(err);
     }
 
-    runAntlr(req.file.filename, req.file.path, req.body.grammarRoot);
+    await runAntlr(req.file.filename, req.file.path, req.body.grammarRoot);
 
     // Storing grammar data in the session
     const grammar = Object();
@@ -126,11 +90,13 @@ export const compileGrammarFile = (req: any, res: any) => {
     grammar.root = req.body.grammarRoot;
     req.session.grammar = grammar;
 
-    return res.status(200).send("OK");
+    return res.status(200).json({
+      tokens: await getGrammarTokens(req.sessionID, grammar.name)
+    });
   });
 };
 
-export const compileGrammarString = (req: any, res: any) => {
+export const compileGrammarString = async (req: any, res: any) => {
   const grammarString: string = req.body.grammar;
 
   /*
@@ -148,22 +114,22 @@ export const compileGrammarString = (req: any, res: any) => {
   const sessionID = req.sessionID;
 
   const _path = path.join(GRAMMAR_STORAGE, sessionID); // Path where to store the grammar related files
-  if (fs.existsSync(_path)) {
-    fs.rmSync(_path, { recursive: true });
-  }
+
   fs.mkdirSync(_path, { recursive: true });
 
   // Write grammar in file
   fs.writeFileSync(path.join(_path, filename), grammarString);
 
   // Run ANTLR on the grammar file
-  runAntlr(filename, path.join(_path, filename), req.body.grammarRoot);
+  await runAntlr(filename, path.join(_path, filename), req.body.grammarRoot);
 
   // Storing grammar data in the session
   const grammar = Object();
   grammar.name = grammarName;
   grammar.root = req.body.grammarRoot;
   req.session.grammar = grammar;
-
-  return res.status(200).send("OK");
+  fs.readdir(_path, (err, files)=>files.forEach(f => console.log(f)))
+  return res.status(200).json({
+    tokens: await getGrammarTokens(req.sessionID, grammar.name)
+  });
 };
---
+++ packages/parser/controllers/grammarTemplatesGenerator.ts
@@ -0,0 +1,54 @@
+import path from "path";
+import fs from "fs";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const ASSETS = process.env.ASSETS || "assets";
+
+// Create grammar related functions
+export const generateParserFile = (grammarName: string, directory: string) : void => {
+    // Generate a function that returns the AST of a given code
+    const parserTemplateFile = fs.readFileSync(`${ASSETS}/parser.hbs`).toString();
+    const parserTemplate = Handlebars.compile(parserTemplateFile);
+  
+    const source = parserTemplate({
+      grammarName: grammarName,
+    });
+    fs.writeFileSync(path.join(directory, "parser.ts"), source);
+};
+
+export const generateLexerFile = (
+    grammarName: string,
+    grammarRoot: string,
+    directory: string,
+  ) : void => {
+    // Generate a function that returns the AST of a given code
+    const lexerTemplateFile = fs.readFileSync(`${ASSETS}/lexer.hbs`).toString();
+    const lexerTemplate = Handlebars.compile(lexerTemplateFile);
+  
+    const source = lexerTemplate({
+      grammarName: grammarName,
+      grammarRoot: grammarRoot,
+    });
+    fs.writeFileSync(path.join(directory, "lexer.ts"), source);
+  };
+
+export const generateAstTreeFile = (
+    grammarName: string,
+    grammarRoot: string,
+    directory: string,
+  ) : void => {
+    // Generate a function that returns the AST of a given code
+    const parseTreeTemplateFile = fs
+      .readFileSync(`${ASSETS}/parseTree.hbs`)
+      .toString();
+    const parseTreeTemplate = Handlebars.compile(parseTreeTemplateFile);
+  
+    const source = parseTreeTemplate({
+      grammarName: grammarName,
+      grammarRoot: grammarRoot,
+    });
+    fs.writeFileSync(path.join(directory, "parseTree.ts"), source);
+  
+  };
